896ec9f6de8dded97a5cfa464e12e9f7fe1de49b
DibyenduDebnath <dibyendu.nir@gmail.com>
2025-05-16 21:50:43 +0530

    JWT, bcrypt, user and video model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import 'dotenv/config';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+});
 
 
 
